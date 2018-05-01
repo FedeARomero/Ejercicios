0113191d6bcb893429ba607e3a2459ebfd06db2e
FedeARomero <fedeandresromero@gmail.com>
2018-04-30 22:06:42 -0300

    Mejoras en los metodos

--- Nivel 1° Parcial/DescubriendoNombresRepetidos/Grupo.java
+++ Nivel 1° Parcial/DescubriendoNombresRepetidos/Grupo.java
@@ -11,43 +11,43 @@ import java.util.SortedMap;
 import java.util.TreeMap;
 
 public class Grupo {
-	private String[] ninios;
+	private String[] nombres;
 	private int nomrepetidos;
-	private Map<String, Integer> nrepetidos;
+	private Map<String, Integer> repeticiones;
 
 	public Grupo(String arch) throws FileNotFoundException {
 		Scanner sc = new Scanner(new File(arch));
 		String[] aux = sc.nextLine().split(" ");
-		ninios = new String[Integer.parseInt(aux[0])];
+		nombres = new String[Integer.parseInt(aux[0])];
 		nomrepetidos = Integer.parseInt(aux[1]);
-		for (int i = 0; i < ninios.length; i++)
-			ninios[i] = sc.nextLine();
+		for (int i = 0; i < nombres.length; i++)
+			nombres[i] = sc.nextLine();
 		sc.close();
 	}
 
 	public void nombresRepetidos(String arch) throws FileNotFoundException {
 		PrintWriter pw = new PrintWriter(new File(arch));
 
-		nrepetidos = new HashMap<String, Integer>();
+		repeticiones = new HashMap<String, Integer>();
 
-		for (int i = 0; i < ninios.length; i++)
-			nrepetidos.put(ninios[i], nrepetidos.containsKey(ninios[i]) ? nrepetidos.get(ninios[i]) + 1 : 1);
+		for (int i = 0; i < nombres.length; i++)
+			repeticiones.put(nombres[i], repeticiones.containsKey(nombres[i]) ? repeticiones.get(nombres[i]) + 1 : 1);
 
-		SortedMap<Integer, String> cantrep = new TreeMap<Integer, String>(java.util.Collections.reverseOrder());
-
-		Iterator<String> iterador1 = nrepetidos.keySet().iterator();
+		SortedMap<Integer, String> nrepeticiones = new TreeMap<Integer, String>(java.util.Collections.reverseOrder());
+		Iterator<String> iterador1 = repeticiones.keySet().iterator();
+		
 		while (iterador1.hasNext()) {
 			String key = (String) iterador1.next();
-			if (nrepetidos.get(key) > 1)
-				cantrep.put(nrepetidos.get(key), key);
+			if (repeticiones.get(key) > 1)
+				nrepeticiones.put(repeticiones.get(key), key);
 		}
 
-		if (cantrep.size() > 0) {
-			Iterator<Integer> iterador2 = cantrep.keySet().iterator();
+		if (nrepeticiones.size() > 0) {
+			Iterator<Integer> iterador2 = nrepeticiones.keySet().iterator();
 			for (int n = 0; n < nomrepetidos && iterador2.hasNext(); n++) {
 				int key = iterador2.next();
-				System.out.println(cantrep.get(key) + " " + key);
-				pw.println(cantrep.get(key) + " " + key);
+				System.out.println(nrepeticiones.get(key) + " " + key);
+				pw.println(nrepeticiones.get(key) + " " + key);
 			}
 		}
 
